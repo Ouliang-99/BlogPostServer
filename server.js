2503bde63914df4c6ee5573d9085f5a4de825f8f
OLEANG <wannachok.job@gmail.com>
2025-01-29 19:56:09 +0700

    fix : deploy to vercel

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import dataRoutes from "./routes/data.js"; // Default Import
+import dataRoutes from "./routes/data.js";
 
-// โหลด Environment Variables
 dotenv.config();
 
-// เริ่มต้น Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// เรียกใช้ API routes
 app.use("/api", dataRoutes);
 
-// เริ่มต้นเซิร์ฟเวอร์
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// ไม่ต้องใช้ app.listen() บน Vercel
+export default app;
